2263d3b739aae25f4fdd216c263dae3761970028
Rodolfo Cristian <rodolfo.figueroa02@inacapmail.cl>
2025-08-17 16:19:34 -0400

    add nuevo archivo

--- app.py
+++ app.py
@@ -63,15 +63,15 @@ def create_app():
     """
     app = Flask(__name__)
     try:
-        client = MongoClient(
-               os.getenv("MONGODB_URI"),
-               tlsCAFile=certifi.where(),
-               serverSelectionTimeoutMS=30000,
-               connectTimeoutMS=30000,
-               socketTimeoutMS=30000,
-               retryWrites=True,
-               retryReads=True,
-           )
+       MONGO_URI = os.getenv("MONGODB_URI")
+       client = MongoClient(
+           MONGO_URI,
+           tls=True,
+           tlsCAFile=certifi.where(),
+           serverSelectionTimeoutMS=15000,
+           connectTimeoutMS=15000,
+           socketTimeoutMS=30000,
+       )
         db = client['peluqueria_bot']
         app.db = db
         app.clients_collection = db.clients
